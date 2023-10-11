a9d12ef8d539bf716e689783438ea809d1df6566
mohammad nishad <mohammadnishad1243.mn@gmail.com>
2023-10-11 12:27:07 +0530

    done converting avif into png

--- src/sections/home/about/image.js
+++ src/sections/home/about/image.js
@@ -2,7 +2,7 @@ import TwiceImage from "@/components/image/twiceImage";
 import React from "react";
 
 const AboutImage = () => {
-  return <TwiceImage img1="/qrdual1.avif" img2="/qrdual2.avif" />;
+  return <TwiceImage img1="/qrdual1.png" img2="/qrdual2.png" />;
 };
 
 export default AboutImage;
